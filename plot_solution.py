54b55b021bb8395446a159454d0c15727e5d2830
Paul Saikko <paul.saikko@cs.helsinki.fi>
2018-12-28 21:06:26 +0200

    Cleanup plotting

--- plot_solution.py
+++ plot_solution.py
@@ -10,16 +10,12 @@ if len(sys.argv) < 2 or not os.path.exists(sys.argv[1]):
     print("need solution file as input")
     exit()
 
-korvatunturi = [68.073611, 29.315278]
-
 locations = {}
 
 with open("nicelist.txt", 'r') as f:
     for line in f:
         tokens = list(map(float, line.split(";")))
         locations[int(tokens[0])] = list(map(float, tokens[1:3]))
-
-locations[1] = korvatunturi
 
 paths = []
 
@@ -30,9 +26,8 @@ with open(sys.argv[1], 'r') as f:
 
 colors = [(random(),random(),random()) for i in range(len(paths))]
 
-plt.ion()
 fix,ax = plt.subplots(figsize=(8, 8))
-for lon in np.linspace(-180,180,10):
+for lon in np.linspace(-180,180,20):
     ax.clear()
     m = Basemap(projection='ortho', resolution=None, lat_0=10, lon_0=lon)
     m.bluemarble(scale=0.5)
@@ -40,4 +35,8 @@ for lon in np.linspace(-180,180,10):
         xys = [ m(locations[i][1],locations[i][0]) for i in path ]
         xs, ys = zip(*xys)
         plt.plot(xs, ys, 'ok', markersize='3', color=colors[i])
+        
+        xs = [x for x in xs if x < 10e10]
+        ys = [y for y in ys if y < 10e10]
+        plt.plot(xs, ys, '-', color=colors[i])
     plt.pause(0.01)
